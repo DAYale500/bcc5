64bd46c54165b1db8cd5c86e0489de6ed9ca6b37
DAYale500 <day_500@hotmail.com>
2025-05-16 22:08:04 -0700

    Tour now ends at New Crew and flashcards are optoinal at end of path chapter.

--- lib/widgets/tour/landing_screen_tour.dart
+++ lib/widgets/tour/landing_screen_tour.dart
@@ -48,7 +48,6 @@ class LandingScreenTour {
           identify: 'mob',
           keyTarget: mobKey,
           enableOverlayTab: true,
-
           contents: [
             TargetContent(
               align: ContentAlign.bottom,
@@ -60,26 +59,9 @@ class LandingScreenTour {
           ],
         ),
         TargetFocus(
-          identify: 'settings',
-          keyTarget: settingsKey,
-          enableOverlayTab: true,
-
-          contents: [
-            TargetContent(
-              align: ContentAlign.bottom,
-              child: Text(
-                TourDescriptions.settings,
-                style: AppTheme.tourDescriptionStyle,
-              ),
-            ),
-          ],
-        ),
-
-        TargetFocus(
           identify: 'search',
           keyTarget: searchKey,
           enableOverlayTab: true,
-
           contents: [
             TargetContent(
               align: ContentAlign.bottom,
@@ -90,38 +72,15 @@ class LandingScreenTour {
             ),
           ],
         ),
-
         TargetFocus(
-          identify: 'newCrew',
-          keyTarget: newCrewKey,
+          identify: 'settings',
+          keyTarget: settingsKey,
           enableOverlayTab: true,
-
-          shape: ShapeLightFocus.RRect,
-          radius: 40, // 👈 shrink the circle size
-          paddingFocus: 6, // 👈 tighter highlight
           contents: [
             TargetContent(
               align: ContentAlign.bottom,
               child: Text(
-                TourDescriptions.newCrew,
-                style: AppTheme.tourDescriptionStyle,
-              ),
-            ),
-          ],
-        ),
-        TargetFocus(
-          identify: 'advanced',
-          keyTarget: advancedRefreshersKey,
-          enableOverlayTab: true,
-
-          shape: ShapeLightFocus.RRect,
-          radius: 40, // 👈 shrink the circle size
-          paddingFocus: 6, // 👈 tighter highlight
-          contents: [
-            TargetContent(
-              align: ContentAlign.bottom,
-              child: Text(
-                TourDescriptions.advanced,
+                TourDescriptions.settings,
                 style: AppTheme.tourDescriptionStyle,
               ),
             ),
@@ -212,23 +171,241 @@ class LandingScreenTour {
             ),
           ],
         ),
+        TargetFocus(
+          identify: 'advanced',
+          keyTarget: advancedRefreshersKey,
+          enableOverlayTab: true,
+          shape: ShapeLightFocus.RRect,
+          radius: 40,
+          paddingFocus: 6,
+          contents: [
+            TargetContent(
+              align: ContentAlign.bottom,
+              child: Text(
+                TourDescriptions.advanced,
+                style: AppTheme.tourDescriptionStyle,
+              ),
+            ),
+          ],
+        ),
+        TargetFocus(
+          identify: 'newCrew',
+          keyTarget: newCrewKey,
+          enableOverlayTab: true,
+          shape: ShapeLightFocus.RRect,
+          radius: 40,
+          paddingFocus: 6,
+          contents: [
+            TargetContent(
+              align: ContentAlign.bottom,
+              child: Text(
+                TourDescriptions.newCrew,
+                style: AppTheme.tourDescriptionStyle,
+              ),
+            ),
+          ],
+        ),
       ],
-
-      // tutorial customizations
-      alignSkip: Alignment.centerRight, // 🔄 Positions the skip button
+      alignSkip: Alignment.centerRight,
       textSkip: "Skip\nTour",
       textStyleSkip: const TextStyle(color: Colors.white),
       focusAnimationDuration: Duration.zero,
       unFocusAnimationDuration: Duration.zero,
-
-      pulseEnable: true, // 👈 disables pulsing circle effect
-
+      pulseEnable: true,
       onFinish: () => logger.i('✅ Tour finished'),
       onSkip: () {
         logger.i('⏭ Tour skipped');
         return true;
       },
     );
+
+    // final tutorial = TutorialCoachMark(
+    //   targets: [
+    //     TargetFocus(
+    //       identify: 'mob',
+    //       keyTarget: mobKey,
+    //       enableOverlayTab: true,
+
+    //       contents: [
+    //         TargetContent(
+    //           align: ContentAlign.bottom,
+    //           child: Text(
+    //             TourDescriptions.mob,
+    //             style: AppTheme.tourDescriptionStyle,
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //     TargetFocus(
+    //       identify: 'settings',
+    //       keyTarget: settingsKey,
+    //       enableOverlayTab: true,
+
+    //       contents: [
+    //         TargetContent(
+    //           align: ContentAlign.bottom,
+    //           child: Text(
+    //             TourDescriptions.settings,
+    //             style: AppTheme.tourDescriptionStyle,
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+
+    //     TargetFocus(
+    //       identify: 'search',
+    //       keyTarget: searchKey,
+    //       enableOverlayTab: true,
+
+    //       contents: [
+    //         TargetContent(
+    //           align: ContentAlign.bottom,
+    //           child: Text(
+    //             TourDescriptions.search,
+    //             style: AppTheme.tourDescriptionStyle,
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+
+    //     TargetFocus(
+    //       identify: 'newCrew',
+    //       keyTarget: newCrewKey,
+    //       enableOverlayTab: true,
+
+    //       shape: ShapeLightFocus.RRect,
+    //       radius: 40, // 👈 shrink the circle size
+    //       paddingFocus: 6, // 👈 tighter highlight
+    //       contents: [
+    //         TargetContent(
+    //           align: ContentAlign.bottom,
+    //           child: Text(
+    //             TourDescriptions.newCrew,
+    //             style: AppTheme.tourDescriptionStyle,
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //     TargetFocus(
+    //       identify: 'advanced',
+    //       keyTarget: advancedRefreshersKey,
+    //       enableOverlayTab: true,
+
+    //       shape: ShapeLightFocus.RRect,
+    //       radius: 40, // 👈 shrink the circle size
+    //       paddingFocus: 6, // 👈 tighter highlight
+    //       contents: [
+    //         TargetContent(
+    //           align: ContentAlign.bottom,
+    //           child: Text(
+    //             TourDescriptions.advanced,
+    //             style: AppTheme.tourDescriptionStyle,
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //     TargetFocus(
+    //       identify: 'bnbHarbor',
+    //       keyTarget: harborKey,
+    //       enableOverlayTab: true,
+    //       shape: ShapeLightFocus.Circle,
+    //       radius: 50,
+    //       paddingFocus: 25,
+    //       contents: [
+    //         TargetContent(
+    //           align: ContentAlign.top,
+    //           child: Text(
+    //             TourDescriptions.bnbHarbor,
+    //             style: AppTheme.tourDescriptionStyle,
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //     TargetFocus(
+    //       identify: 'bnbCourses',
+    //       keyTarget: coursesKey,
+    //       enableOverlayTab: true,
+    //       shape: ShapeLightFocus.Circle,
+    //       radius: 50,
+    //       paddingFocus: 25,
+    //       contents: [
+    //         TargetContent(
+    //           align: ContentAlign.top,
+    //           child: Text(
+    //             TourDescriptions.bnbCourses,
+    //             style: AppTheme.tourDescriptionStyle,
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //     TargetFocus(
+    //       identify: 'bnbParts',
+    //       keyTarget: partsKey,
+    //       enableOverlayTab: true,
+    //       shape: ShapeLightFocus.Circle,
+    //       radius: 50,
+    //       paddingFocus: 20,
+    //       contents: [
+    //         TargetContent(
+    //           align: ContentAlign.top,
+    //           child: Text(
+    //             TourDescriptions.bnbParts,
+    //             style: AppTheme.tourDescriptionStyle,
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //     TargetFocus(
+    //       identify: 'bnbTools',
+    //       keyTarget: toolsKey,
+    //       enableOverlayTab: true,
+    //       shape: ShapeLightFocus.Circle,
+    //       radius: 50,
+    //       paddingFocus: 20,
+    //       contents: [
+    //         TargetContent(
+    //           align: ContentAlign.top,
+    //           child: Text(
+    //             TourDescriptions.bnbTools,
+    //             style: AppTheme.tourDescriptionStyle,
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //     TargetFocus(
+    //       identify: 'bnbDrills',
+    //       keyTarget: drillsKey,
+    //       enableOverlayTab: true,
+    //       shape: ShapeLightFocus.Circle,
+    //       radius: 50,
+    //       paddingFocus: 20,
+    //       contents: [
+    //         TargetContent(
+    //           align: ContentAlign.top,
+    //           child: Text(
+    //             TourDescriptions.bnbDrills,
+    //             style: AppTheme.tourDescriptionStyle,
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ],
+
+    //   // tutorial customizations
+    //   alignSkip: Alignment.centerRight, // 🔄 Positions the skip button
+    //   textSkip: "Skip\nTour",
+    //   textStyleSkip: const TextStyle(color: Colors.white),
+    //   focusAnimationDuration: Duration.zero,
+    //   unFocusAnimationDuration: Duration.zero,
+
+    //   pulseEnable: true, // 👈 disables pulsing circle effect
+
+    //   onFinish: () => logger.i('✅ Tour finished'),
+    //   onSkip: () {
+    //     logger.i('⏭ Tour skipped');
+    //     return true;
+    //   },
+    // );
 
     await markSeen();
     if (!state.mounted) return;
